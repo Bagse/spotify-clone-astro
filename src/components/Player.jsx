3948f8bd9e48b9baa12442fe22d3e344bc52fd42
bagse <102260190+Bagse@users.noreply.github.com>
2023-11-20 22:18:50 -0500

    added functionality to the player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
+import { useState } from "react";
+import { usePlayerStore } from "../store/playerStore";
+
+export const Pause = ({ className }) => (
+  <svg
+    className={className}
+    role="img"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    viewBox="0 0 16 16"
+  >
+    <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
+  </svg>
+);
+
+export const Play = ({ className }) => (
+  <svg
+    className={className}
+    role="img"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    viewBox="0 0 16 16"
+  >
+    <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
+  </svg>
+);
+
+const CurrentSong = ({ image, title, artists }) => {
+  return (
+    <div className="flex items-center gap-5 relative overflow-hidden">
+      <picture className="h-16 w-16 rounded-md shadow-lg overflow-hidden bg-zinc-800">
+        <img src={image} alt={title} />
+      </picture>
+
+      <div className="flex flex-col">
+        <h3 className="font-semibold block text-sm">{title}</h3>
+        <span className="text-xs opacity-80">{artists?.join(", ")}</span>
+      </div>
+    </div>
+  );
+};
 
 function Player() {
+  const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(
+    (state) => state
+  );
+  const audioRef = useRef();
+
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic;
+    if (song) {
+      const src = `./music/${playlist?.id}/0${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.play();
+    }
+  }, [currentMusic]);
+
+  useEffect(() => {
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying]);
+
+  const handleClick = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   return (
-    <div>Player</div>
-  )
+    <div className="flex flex-row justify-between w-full px-4 z-50">
+      <div>
+        <CurrentSong {...currentMusic.song} />
+      </div>
+
+      <div className="grid place-content-center gap-4 flex-1">
+        <div className="flex justify-center">
+          <button className="bg-white rounded-full p-2" onClick={handleClick}>
+            {isPlaying ? <Pause /> : <Play />}
+          </button>
+        </div>
+      </div>
+
+      <div className="grid place-content-center">volumen</div>
+
+      <audio ref={audioRef} />
+    </div>
+  );
 }
 
-export default Player
+export default Player;
